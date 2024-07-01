ae9d31212571ea90f893c63c2ce5c817649619a0
itsgerliz <gerworks.hs@gmail.com>
2024-07-02 00:10:15 +0200

    Pending to get content of data stream defined by file path and fix malloc issue

--- main.c
+++ main.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* getPath() {
-	
-	//Error I got the first time in the following line:
-	//If you do not assign the char pointer (path) to some space in memory...
-	//You will get a segmentation fault when running fgets cause fgets does not know where to save data read from data stream (stdin)
-	//To avoid the previous error we need to assign to char pointer a pointer returned by malloc, malloc allocates memory for this pointer
-	
-	char* path = malloc((sizeof(char) * 100));
+//Error I got the first time in the following line:
+//If you do not assign the char pointer (path) to some space in memory...
+//You will get a segmentation fault when running fgets cause fgets does not know where to save data read from data stream (stdin)
+//To avoid the previous error we need to assign to char pointer a pointer returned by malloc, malloc allocates memory for this pointer	
+
+char* buffer = malloc((sizeof(char) * 100));
+
+char* getPath(char* path) {
 	
 	fprintf(stdout, "Enter the file path: ");
 	
 	if (fgets(path, 100, stdin) == NULL) {
 		fprintf(stdout, "\n");
 		fprintf(stderr, "Error reading from data stream\n");
-		char* aborted = NULL;
+		auto char* aborted = NULL;
 		return aborted;
 	} else {
 		while (strchr(path, '\n') != NULL) {
@@ -31,7 +31,7 @@ char* getPath() {
 
 int main() {
 	
-	char* result = getPath();
+	char* result = getPath(buffer);
 	
 	if (result == NULL) {
 		fprintf(stderr, "Aborting...\n");
@@ -47,7 +47,14 @@ int main() {
 		fprintf(stderr, "Aborting...\n");
 		return 1;
 	} else {
+		fileptr = fopen(result, "r");
 		fprintf(stdout, "File was opened succesfully, reading...\n");
+		free(buffer); //Freeing memory allocated in buffer by malloc function before
+		fprintf(stdout, "--BEGIN OF FILE--\n");
+		//Get contents from stream function goes here		
+		fprintf(stdout, "--END OF FILE--\n");
+		fprintf(stdout, "Exiting...\n");
+		return 0;
 	}
 
 }

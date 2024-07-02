0632170d7cd0a36f446a1f74a3efe20dc031ccad
itsgerliz <gerworks.hs@gmail.com>
2024-07-02 12:41:08 +0200

    Program finished and it works great

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//Error I got the first time in the following line:
-//If you do not assign the char pointer (path) to some space in memory...
+//Error I got the first time in the following line: buffer char pointer
+//If you do not assign the char pointer (buffer) to some space in memory...
 //You will get a segmentation fault when running fgets cause fgets does not know where to save data read from data stream (stdin)
 //To avoid the previous error we need to assign to char pointer a pointer returned by malloc, malloc allocates memory for this pointer	
 
-char* buffer = malloc((sizeof(char) * 100));
-
-char* getPath(char* path) {
+char* getPath(char* whereToSave) {
 	
 	fprintf(stdout, "Enter the file path: ");
 	
-	if (fgets(path, 100, stdin) == NULL) {
+	if (fgets(whereToSave, 100, stdin) == NULL) {
 		fprintf(stdout, "\n");
 		fprintf(stderr, "Error reading from data stream\n");
 		auto char* aborted = NULL;
 		return aborted;
 	} else {
-		while (strchr(path, '\n') != NULL) {
-			auto char* i = strchr(path, '\n');
+		while (strchr(whereToSave, '\n') != NULL) {
+			auto char* i = strchr(whereToSave, '\n');
 			*i = '\0';
 		}
 		fprintf(stdout, "OK!\n");
-		return path;
+		return whereToSave;
 	}
 
 }
 
+int getStreamChar(FILE* dataStream, char* dataTarget) {
+	*dataTarget = fgetc(dataStream);
+	if (*dataTarget != EOF) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
 int main() {
+
+	char* buffer = malloc((sizeof(char) * 100)); //Allocate 100 bytes of memory for path
 	
-	char* result = getPath(buffer);
-	
+	char* result = getPath(buffer); //Save the resulting path
+
 	if (result == NULL) {
 		fprintf(stderr, "Aborting...\n");
 		return 1;
@@ -48,11 +57,19 @@ int main() {
 		return 1;
 	} else {
 		fileptr = fopen(result, "r");
+		free(buffer);
 		fprintf(stdout, "File was opened succesfully, reading...\n");
-		free(buffer); //Freeing memory allocated in buffer by malloc function before
+		
+		auto char* tempChar = malloc(sizeof(char));		
+		
 		fprintf(stdout, "--BEGIN OF FILE--\n");
-		//Get contents from stream function goes here		
+		
+		do {
+			fprintf(stdout, "%s", tempChar);
+		} while (getStreamChar(fileptr, tempChar) == 0);
+
 		fprintf(stdout, "--END OF FILE--\n");
+		
 		fprintf(stdout, "Exiting...\n");
 		return 0;
 	}
